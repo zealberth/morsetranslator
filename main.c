72ec639fb8571bc383663427a07af4d9aa0201f4
Alberth <j.alberth@hotmail.com>
2017-02-24 09:06:30 -0300

    Inclusão do menu

--- main.c
+++ main.c
@@ -10,27 +10,59 @@
 
 int main()
 {
-	char str[200];
-	int i = 0;
-  char opt;
-  menu();
-	printf("\t\tDigite um texto para codificar:\n");
-	scanf("%[^\t\n]s",str);
-	printf("\n\n");
+  	menu();
 
-	// getch();
+  	while (1)
+  	{
+  		char str[300];
+  		int i = 0;
+  		char opt;
+  		int item = 0;
+  		printf("\t\tEscolha o item desejado:\n");
+  		printf("1 - Mostrar tabela de codigos\n");
+  		printf("2 - Traduzir algum texto (limitado a 300 caracteres)\n");
+  		printf("3 - Encerrar programa\n");
+  		printf("Opcao: ");
+  		scanf("%d", &item);
+  		fflush(stdin);
+    	__fpurge(stdin);
 
-	while(str[i])
-    {
-        printf("%s ", morse(str[i]));
-      	i++;
-    }
-    printf("\n");
-    printf("Deseja salvar a seu texto e a tradução? [S/N] \n");
-    scanf("%c",&opt);
-    if(opt=='s'){
-      salvaTxt("teste.txt", str);
-    }
+  		switch(item)
+  		{
+  			case 1:
+  				mostraMorse();
+  				break;
+  			case 2:
+	  			printf("\n\n\t\tDigite um texto para codificar:\n");
+				scanf("%[^\t\n]s",str);
+  				fflush(stdin);
+    			__fpurge(stdin);
+				printf("\n");
+
+				while(str[i])
+			    {
+			        printf("%s ", morse(str[i]));
+			      	i++;
+			    }
+			    printf("\n");
+			    printf("Deseja salvar a seu texto e a tradução? [S/N] \n");
+			    scanf(" %c",&opt);	
+  				fflush(stdin);
+    			__fpurge(stdin);
+			    opt = toupper(opt);
+			    if(opt=='S'){
+			      salvaTxt("morse.txt", str);
+			    }
+  				break;
+  			case 3:
+  				goto exit;
+  				break;
+  			default:
+  				printf("\nItem incorreto, tente novamente!\n\n\n");
+  		}
+  	}
+
+  	exit:
 
     return(1);
 }
